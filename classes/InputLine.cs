278a553ae594a641f8fee81117426350e4098cb9
Maksim Gurianov <maksim.solvent@gmail.com>
2019-04-13 16:07:16 +0300

    year tags parsing added

--- classes/InputLine.cs
+++ classes/InputLine.cs
@@ -18,25 +18,30 @@ namespace pawrser.classes {
 			"05_Go",    "Good Named",  "SD",
 			"06_Roku",  "Hardsub",     "Softsub",
 			"07_Nana",  "HD",          "VHS & DVD Rip, etc",
-			"08_Hachi"
+			"08_Hachi", "Live!"
 		};
 
 		private static readonly IEnumerable<string> TagsToSkip = new List<string>() {
 			"", "mnt", "anime"
 		};
 
+		private bool IsTag(string piece) {
+			return YearRegEx.IsMatch(piece) || KnownTags.Contains(piece);
+		}
+
 		public InputLine(string path) {
 			var pieces = path.Split('/').Where(x => !TagsToSkip.Contains(x));
 			IsFile = pieces.FirstOrDefault() != null
-				? regex.IsMatch(pieces.LastOrDefault())
+				? FileExtentionRegEx.IsMatch(pieces.LastOrDefault())
 				: false;
-			Tags = pieces.Where(x => KnownTags.Contains(x));
+			Tags = pieces.Where(x => IsTag(x));
 			Episode = IsFile ? pieces.LastOrDefault() : "";
-			Title = pieces.Where(x => x != Episode && !KnownTags.Contains(x)).FirstOrDefault()
+			Title = pieces.Where(x => x != Episode && !IsTag(x)).FirstOrDefault()
 				?? "Random Pieces";
 		}
 
-		private static Regex regex = new Regex(@"\.[A-Za-z0-9]{1,4}$");
+		private static Regex FileExtentionRegEx = new Regex(@"\.[A-Za-z0-9]{1,4}$");
+		private static Regex YearRegEx = new Regex(@"^(19|20)\d{2}$");
 	}
 
 }
